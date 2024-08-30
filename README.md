df4ec6224b48362c172f3cd037083e7556ac5cf1
Alex Tsai <iamalex33329@gmail.com>
2024-08-30 14:05:30 +0800

    8️⃣30, 2024

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ This repository contains my solutions to [LeetCode](https://leetcode.com/) probl
 | Difficulty | Solved Problems |
 |:----------:|:---------------:|
 |    Easy    |       75        |
-|   Medium   |       75        |
+|   Medium   |       76        |
 |    Hard    |        6        |
-| **Total**  |     **156**     |
+| **Total**  |     **157**     |
---
+++ src/medium/Problem875.java
@@ -0,0 +1,32 @@
+package medium;
+
+
+public class Problem875 {
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int left = 1;
+        int right = 0;
+
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int hours = 0;
+
+            for (int pile : piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+
+            if (hours <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
